5f7b676d2025cbf8eb1cd8b72a8777bc02f61442
DannyGrimmig <dannygrimmig17@gmail.com>
2024-07-01 21:08:24 -0400

    flat list: style updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    OutfitRegular: require("../assets/fonts/Outfit-Regular.ttf"),
+    OutfitLight: require("../assets/fonts/Outfit-Thin.ttf"),
+    OutfitSemiBold: require("../assets/fonts/Outfit-SemiBold.ttf"),
+  });
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
-import { Text, View } from "react-native";
+import * as React from "react";
+
+import {
+  TouchableOpacity,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+} from "react-native";
 
 export default function Index() {
+  const [search, setSearch] = React.useState("");
+  const [filteredCourses, setFilteredCourses] =
+    React.useState<Course[]>(fakeCourses);
+
+  const updateSearch = (text: string) => {
+    setSearch(text);
+    const filtered = fakeCourses.filter((course) =>
+      course.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredCourses(filtered);
+  };
+
   return (
     <View
       style={{
@@ -9,7 +30,71 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <FlatList
+        data={filteredCourses}
+        renderItem={({ item }) => <Course course={item} />}
+        style={{
+          backgroundColor: "white",
+          width: "100%",
+          paddingHorizontal: 24,
+        }}
+      />
     </View>
   );
 }
+
+type Course = {
+  id: string;
+  name: string;
+  location: string;
+};
+
+function Course(props: { course: Course }) {
+  const { course } = props;
+
+  const onCoursePress = (course: Course) => {
+    Alert.alert(course.name, course.location);
+  };
+
+  return (
+    <TouchableOpacity
+      style={styles.course_card}
+      onPress={() => onCoursePress(course)}
+    >
+      <Text style={styles.course_name}>{course.name}</Text>
+      <Text style={styles.course_city}>{course.location}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  course_card: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    marginVertical: 4,
+    borderColor: "black",
+    borderBottomWidth: 0.5,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+  },
+  course_name: {
+    fontSize: 16,
+    fontFamily: "OutfitRegular",
+  },
+  course_city: {
+    fontFamily: "OutfitLight",
+  },
+});
+
+const fakeCourses: Course[] = [
+  { id: "1", name: "Pebble Beach", location: "Monteray, CA" },
+  { id: "2", name: "Augusta National", location: "Augusta, GA" },
+  { id: "3", name: "Baltusrol Golf Club", location: "Springfield, NJ" },
+  { id: "4", name: "Bethpage Black", location: "Bethpage, NY" },
+  { id: "5", name: "Pine Valley", location: "Pine Valley, NJ" },
+  { id: "6", name: "Shinnecock Hills", location: "Southampton, NY" },
+  { id: "7", name: "Oakmont", location: "Oakmont, PA" },
+  { id: "8", name: "Merion (East)", location: "Admore, PA" },
+];
