1412c37d1b39c7e6a55cd22fdc078452d46e5413
DannyGrimmig <dannygrimmig17@gmail.com>
2024-07-05 14:08:21 -0400

    convert to ts, add test course

--- api/courses.ts
+++ api/courses.ts
@@ -1,5 +1,10 @@
 import { Course } from "@/definitions/course";
 import { CHATHAM_SEASIIDE_LINKS } from "@/api/courses/chatham_seaside_links";
 import { PINE_MEADOWS_GOLF } from "./courses/pine_meadows_golf";
+import { TEST_226 } from "./courses/226_test";
 
-export const COURSES: Course[] = [CHATHAM_SEASIIDE_LINKS, PINE_MEADOWS_GOLF];
+export const COURSES: Course[] = [
+  CHATHAM_SEASIIDE_LINKS,
+  PINE_MEADOWS_GOLF,
+  TEST_226,
+];
